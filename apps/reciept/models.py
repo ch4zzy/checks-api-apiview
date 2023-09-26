abf03ed76d7ae566001d9782d602e68b24d68e4d
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-09-26 13:18:29 +0300

    main app

--- apps/reciept/models.py
+++ apps/reciept/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from .constants import RecieptType, StatusType
+
+
+class Printer(models.Model):
+    """
+    Model representing a printer.
+    """
+
+    name = models.CharField(max_length=50)
+    api_key = models.CharField(max_length=50, unique=True)
+    check_type = models.CharField(max_length=10, choices=RecieptType.choices)
+    point_id = models.IntegerField()
+
+
+class Check(models.Model):
+    """
+    Model representing a check.
+    """
+
+    printer = models.ForeignKey(Printer, on_delete=models.CASCADE, related_name="checks")
+    check_type = models.CharField(max_length=10, choices=RecieptType.choices)
+    order = models.JSONField()
+    status = models.CharField(max_length=10, choices=StatusType.choices)
+    pdf_file = models.FileField(blank=True)
